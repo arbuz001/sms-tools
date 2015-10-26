d2675e97728428dc16efd155cd7f4cfe8f98696e
alexey.arbuzov <alexey.arbuzov@gmail.com>
2015-10-25 18:27:42 -0700

    adding codes for A4 assignment - Part2

---
+++ workspace/A4/A4Part2.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import numpy as np
+import math
+from scipy.signal import get_window
+import matplotlib.pyplot as plt
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../software/models/'))
+from stft import stft
+import utilFunctions as UF
+eps = np.finfo(float).eps
+
+
+"""
+A4-Part-2: Measuring noise in the reconstructed signal using the STFT model 
+
+Write a function that measures the amount of noise introduced during the analysis and synthesis of a 
+signal using the STFT model. Use SNR (signal to noise ratio) in dB to quantify the amount of noise. 
+Use the stft() function in stft.py to do an analysis followed by a synthesis of the input signal.
+
+A brief description of the SNR computation can be found in the pdf document (A4-STFT.pdf, in Relevant 
+Concepts section) in the assignment directory (A4). Use the time domain energy definition to compute
+the SNR.
+
+With the input signal and the obtained output, compute two different SNR values for the following cases:
+
+1) SNR1: Over the entire length of the input and the output signals.
+2) SNR2: For the segment of the signals left after discarding M samples from both the start and the 
+end, where M is the analysis window length. Note that this computation is done after STFT analysis 
+and synthesis.
+
+The input arguments to the function are the wav file name including the path (inputFile), window 
+type (window), window length (M), FFT size (N), and hop size (H). The function should return a python 
+tuple of both the SNR values in decibels: (SNR1, SNR2). Both SNR1 and SNR2 are float values. 
+
+Test case 1: If you run your code using piano.wav file with 'blackman' window, M = 513, N = 2048 and 
+H = 128, the output SNR values should be around: (67.57748352378475, 304.68394693221649).
+
+Test case 2: If you run your code using sax-phrase-short.wav file with 'hamming' window, M = 512, 
+N = 1024 and H = 64, the output SNR values should be around: (89.510506656299285, 306.18696700251388).
+
+Test case 3: If you run your code using rain.wav file with 'hann' window, M = 1024, N = 2048 and 
+H = 128, the output SNR values should be around: (74.631476225366825, 304.26918192997738).
+
+Due to precision differences on different machines/hardware, compared to the expected SNR values, your 
+output values can differ by +/-10dB for SNR1 and +/-100dB for SNR2.
+"""
+
+def computeSNR(inputFile, window, M, N, H):
+    """
+    Input:
+            inputFile (string): wav file name including the path 
+            window (string): analysis window type (choice of rectangular, triangular, hanning, hamming, 
+                    blackman, blackmanharris)
+            M (integer): analysis window length (odd positive integer)
+            N (integer): fft size (power of two, > M)
+            H (integer): hop size for the stft computation
+    Output:
+            The function should return a python tuple of both the SNR values (SNR1, SNR2)
+            SNR1 and SNR2 are floats.
+    """
+    x = UF.wavread(inputFile)[1]
+    w = get_window(window, M)
+
+    xSynth = stft(x, 1.0, w, N, H)
+
+    eSignal1 = sum(x**2)
+    eNoise1 = sum((x-xSynth)**2)
+    SNR1 = 10.0*np.log10(eSignal1/eNoise1)
+
+    x2 = x[M:len(x)-M]
+    xSynth2 = xSynth[M:len(xSynth)-M]
+
+    eSignal2 = sum(x2**2)
+    eNoise2 = sum((x2-xSynth2)**2)
+    SNR2 = 10.0*np.log10(eSignal2/eNoise2)
+
+    return (SNR1,SNR2)
+
+# inputFile = "/home/alex/Documents/sms-tools/sounds/sax-phrase-short.wav"
+
+# M = 512
+# N = 1024
+# H = 64
+# window = "hamming"
+
+# (SNR1,SNR2) = computeSNR(inputFile, window, M, N, H)
